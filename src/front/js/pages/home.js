e5c85cdc361a8405959cb1c414831e6cffcc02ca
Jorge Moreno <jjmorenoaudio@gmail.com>
2025-01-14 17:21:56 +0000

    carruceles del home listos a menos que quieran filtrar de otra forma

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,11 +5,27 @@ import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const sortByMostRecent = (a, b) => new Date(b.created_at) - new Date(a.created_at);
+	const sortByUpcoming = (a, b) => new Date(a.date) - new Date(b.date);
+	const filterFreeEvents = (event) => event.price === 0;
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Eventos</h1>
-			<Carousel />
+		<div className="container">
+<h3>Recently Added</h3>
+			<Carousel
+				title="Most Recently Added Events"
+				sort={sortByMostRecent}
+			/>
+<h3>Upcoming events</h3>
+			<Carousel
+				title="Upcoming Events"
+				sort={sortByUpcoming}
+			/>
+<h3>Free</h3>
+			<Carousel
+				title="Free Events"
+				filter={filterFreeEvents}
+			/>
 		</div>
 	);
 };
