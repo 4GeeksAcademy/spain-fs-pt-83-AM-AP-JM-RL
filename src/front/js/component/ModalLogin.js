21e103b96b3847b172275b52f4734a203980105f
Aaron Pitarch <aapitarch@gmail.com>
2025-01-16 17:59:47 +0000

    arreglo useState

--- src/front/js/component/ModalLogin.js
+++ src/front/js/component/ModalLogin.js
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 export const ModalLogin = ({ show, onClose }) => {
     const { actions } = useContext(Context);
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
