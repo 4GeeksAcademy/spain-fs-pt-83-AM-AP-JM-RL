4f9b8e450615a5b3d50704729e0632ae71a85831
adrims91 <adrims91@gmail.com>
2025-01-16 17:48:04 +0000

    arreglado conflicto

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,26 +1,13 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 
-	  store: {
-		users: [],
-		events: [],
-		favorites:[]
-	  },
-	  actions: {
-		register: async (email, password) => {
-			try {
-				const response = await fetch(`${process.env.BACKEND_URL}api/users`,
-					{
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json'
-						},
-						body: JSON.stringify({email, password})
-					}
-				);
 		store: {
+			users: [],
 			events: [],
+			favorites: []
 		},
+
+
 		actions: {
 			register: async (email, password) => {
 				try {
@@ -120,41 +107,41 @@ const getState = ({ getStore, getActions, setStore }) => {
 				} catch (error) {
 					console.error(error)
 				}
-			}
+			},
 
 
-		getEvents: async () => {
-		  const store = getStore();
-		  if (store.events.length === 0) {
-			try {
-			  const resp = await fetch(process.env.BACKEND_URL + "/api/events");
-			  const respJson = await resp.json();
-			  setStore({ events: respJson });
-			} catch (error) {
-			  console.error("Error fetching events:", error);
-			}
-		  }
+			getEvents: async () => {
+				const store = getStore();
+				if (store.events.length === 0) {
+					try {
+						const resp = await fetch(process.env.BACKEND_URL + "/api/events");
+						const respJson = await resp.json();
+						setStore({ events: respJson });
+					} catch (error) {
+						console.error("Error fetching events:", error);
+					}
+				}
 
-		},
+			},
 
-		// addFavorite: async (item) => {
-		// 	const { favorites } = getStore();
-		// 	if (!favorites.length) {
-		// 		try {
-		// 		  const resp = await fetch(process.env.BACKEND_URL + "/api/favorites");
-		// 		  const respJson = await resp.json();
-		// 		  setStore({ events: respJson });
-		// 		} catch (error) {
-		// 		  console.error("Error fetching events:", error);
-		// 		}
-		// 	  }
-		// },
-		
-		removeFavorite: (id) => {
-			const { favorites } = getStore();
-			setStore({ favorites: event.id});
-		}
-	  },
+			// addFavorite: async (item) => {
+			// 	const { favorites } = getStore();
+			// 	if (!favorites.length) {
+			// 		try {
+			// 		  const resp = await fetch(process.env.BACKEND_URL + "/api/favorites");
+			// 		  const respJson = await resp.json();
+			// 		  setStore({ events: respJson });
+			// 		} catch (error) {
+			// 		  console.error("Error fetching events:", error);
+			// 		}
+			// 	  }
+			// },
+
+			removeFavorite: (id) => {
+				const { favorites } = getStore();
+				setStore({ favorites: event.id });
+
+			},
 			// addFavorite: (item) => {
 			// 	const { favorites } = getStore();
 			// 	if (!favorites.some(fav => fav.id === item.id && fav.type === item.type)) {
