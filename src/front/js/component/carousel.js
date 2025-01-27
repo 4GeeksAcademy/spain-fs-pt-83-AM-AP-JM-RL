59b576b9e115e70c74079554d8771eb4e8e2a010
adrims91 <adrims91@gmail.com>
2025-01-27 19:59:55 +0000

    1

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -14,7 +14,6 @@ export const Carousel = ({ filter, sort, title, id }) => {
     return filteredEvents;
   };
 
-
   const groupEvents = (events, chunkSize) => {
     const chunks = [];
     for (let i = 0; i < events.length; i += chunkSize) {
@@ -71,26 +70,17 @@ export const Carousel = ({ filter, sort, title, id }) => {
                               </Link>
                               {store.favorites.some((fav) => fav.event_id === event.id) ? (
                                 <i
-                                  onClick={() => {
-
-                                    const favorite = store.favorites.find((fav) => fav.event_id === event.id);
-                                    if (favorite && event.id) {
-                                      actions.addFavorite(event.id);
-                                    }
-                                  }}
+                                  onClick={() => actions.removeFavorite(store.favorites.find((fav) => fav.event_id === event.id).id)}
                                   className="fa-solid fa-star text-warning"
                                   style={{ cursor: "pointer" }}
                                 ></i>
                               ) : (
                                 <i
-                                  onClick={() => {
-                                    actions.removeFavorite(event.id);
-                                  }}
+                                  onClick={() => actions.addFavorite(store.user.id, event.id)}
                                   className="fa-regular fa-star"
                                   style={{ cursor: "pointer" }}
                                 ></i>
                               )}
-
                             </div>
                           </div>
                         </div>
